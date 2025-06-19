815981f2417770372fb9630471368ba3def3bab5
Dustin Smith <dusmith0@tamu.edu>
2025-06-19 10:43:42 -0500

    I moved some files

--- Misc. Junk Code/Climate Initial Plots.R
+++ Misc. Junk Code/Climate Initial Plots.R
@@ -13,7 +13,7 @@ library("dplyr")
 
 
 ## Getting the data
-  path <- file.path("C:", "Users", "dustin.smith", "OneDrive - Lubbock Independent School District",
+  path <- file.path("C:", "Users", "dustin.smith", "OneDrive - Lubbock Independent School District","Pictures",
                   "Documents","Codes","R","Climate data and Water Levels","Original Data Sets",fsep = "\\")
   type <- file.path("Climate Data",fsep = "\\")
   data <- read.csv(paste(path,type,"San Antonio Airport (SAT) climate data (1948 to 2025).csv",sep = "\\"))
